10dd692cb0c2604dea4ba5b86ff32d33a33321cc
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:20:21 +0530

    Delete Telegram/modules/security directory

--- Telegram/modules/security/firewall.py
+++
@@ -1,9 +0,0 @@
-import time
-
-BLACKLIST = {}
-
-def rate_limit(user_id):
-    if user_id in BLACKLIST and time.time() - BLACKLIST[user_id] < 10:
-        return True
-    BLACKLIST[user_id] = time.time()
-    return False
