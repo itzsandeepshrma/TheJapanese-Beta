351fe50dfae0f38806dc68616e61d7d866963eac
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:02:43 +0530

    Update mongo.py

--- Telegram/database/mongo.py
+++ Telegram/database/mongo.py
@@ -1,1 +1,7 @@
+from pymongo import MongoClient
 
+client = MongoClient("mongodb://localhost:27017/")
+db = client["TheJapaneseDB"]
+
+def insert_user(user_id, username):
+    db.users.insert_one({"user_id": user_id, "username": username})
