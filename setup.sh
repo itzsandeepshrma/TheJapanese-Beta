722ae4aa0f49102ae464334180aba047708eaea2
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 17:47:00 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "🔹 Installing Python Dependencies..."
-pip install -r requirements.txt
-
-echo "🔹 Installing Node.js Dependencies..."
-cd TheJapanese/Instagram && npm install
-cd ../WhatsApp && npm install
-
-echo "✅ Setup Complete!"
