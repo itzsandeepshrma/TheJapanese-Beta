647c713501180710130e4a8b6cc53b6bddb797b4
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:18:49 +0530

    Delete Instagram/security directory

--- Instagram/security/firewall.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-    windowMs: 10 * 1000, 
-    max: 5, 
-    message: "🚫 Too many requests! Please slow down."
-});
-
-module.exports = limiter;
