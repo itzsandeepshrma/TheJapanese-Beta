4b92eb9709c43de39233ece87b4f5428a5033299
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:00:15 +0530

    Update bot.js

--- Instagram/core/bot.js
+++ Instagram/core/bot.js
@@ -1,1 +1,10 @@
+const { IgApiClient } = require("instagram-private-api");
+const ig = new IgApiClient();
 
+async function login() {
+    ig.state.generateDevice("your_username");
+    await ig.account.login("your_username", "your_password");
+    console.log("✅ Instagram Bot is Running...");
+}
+
+login();
