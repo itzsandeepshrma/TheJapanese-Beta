c77e7f15865d571c695846c7cecf4b0ba60edd5f
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:21:03 +0530

    Delete WhatsApp/core directory

--- WhatsApp/core/bot.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("whatsapp-web.js");
-const qrcode = require("qrcode-terminal");
-
-const client = new Client();
-client.on("qr", qr => qrcode.generate(qr, { small: true }));
-client.on("ready", () => console.log("✅ WhatsApp Bot is Running..."));
-
-client.initialize();
