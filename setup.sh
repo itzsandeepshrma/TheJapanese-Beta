748be9064cccad88aa3547c2a374a3767fa384c5
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 14:57:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+echo "🔹 Installing Python Dependencies..."
+pip install -r requirements.txt
 
+echo "🔹 Installing Node.js Dependencies..."
+cd TheJapanese/Instagram && npm install
+cd ../WhatsApp && npm install
+
+echo "✅ Setup Complete!"
