c146940aba121ade478518b6e6903cab73902abe
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:20:07 +0530

    Delete Telegram/database directory

--- Telegram/database/mongo.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["TheJapaneseDB"]
-
-def insert_user(user_id, username):
-    db.users.insert_one({"user_id": user_id, "username": username})
