8c3e26f02c49eb48fdfe1e4ab552361cd2335c4c
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:00:49 +0530

    Update firewall.js

--- Instagram/security/firewall.js
+++ Instagram/security/firewall.js
@@ -1,1 +1,9 @@
+const rateLimit = require("express-rate-limit");
 
+const limiter = rateLimit({
+    windowMs: 10 * 1000, 
+    max: 5, 
+    message: "🚫 Too many requests! Please slow down."
+});
+
+module.exports = limiter;
