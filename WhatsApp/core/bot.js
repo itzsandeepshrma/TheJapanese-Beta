a366cd69c2ac6d71e1c8f9faca93fe9e53116034
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:01:22 +0530

    Update bot.js

--- WhatsApp/core/bot.js
+++ WhatsApp/core/bot.js
@@ -1,1 +1,8 @@
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
+const client = new Client();
+client.on("qr", qr => qrcode.generate(qr, { small: true }));
+client.on("ready", () => console.log("✅ WhatsApp Bot is Running..."));
+
+client.initialize();
