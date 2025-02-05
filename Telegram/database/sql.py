eb518b04bf6a55d27ae3e343a34f9d2634f42fd7
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-05 15:03:06 +0530

    Update sql.py

--- Telegram/database/sql.py
+++ Telegram/database/sql.py
@@ -1,1 +1,14 @@
+import mysql.connector
 
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="yourpassword",
+    database="TheJapaneseDB"
+)
+
+cursor = db.cursor()
+
+def insert_user(user_id, username):
+    cursor.execute("INSERT INTO users (user_id, username) VALUES (%s, %s)", (user_id, username))
+    db.commit()
