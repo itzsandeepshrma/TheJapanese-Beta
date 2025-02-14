60e5af950fefefd3b08892c0aa726ad4922cd47d
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-14 23:26:27 +0530

    Update start.sh JAPANESE ✨

--- start.sh
+++ start.sh
@@ -1,1 +1,28 @@
+#!/bin/sh
 
+case "$PLATFORM" in
+  discord)
+    echo "Starting Discord bot..."
+    node Discord/bot.js
+    ;;
+  whatsapp)
+    echo "Starting WhatsApp bot..."
+    node WhatsApp/index.js
+    ;;
+  telegram)
+    echo "Starting Telegram bot..."
+    python3 Telegram/bot.py
+    ;;
+  instagram)
+    echo "Starting Instagram bot..."
+    node Instagram/bot.js
+    ;;
+  twitter)
+    echo "Starting Twitter bot..."
+    node X/bot.js
+    ;;
+  *)
+    echo "Error: PLATFORM variable is not set correctly!"
+    exit 1
+    ;;
+esac
