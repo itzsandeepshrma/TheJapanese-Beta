11fb7c667ebd31f11f27e87417d1b259aaa70816
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-02-15 11:44:34 +0530

    Update bot.js update soon

--- Discord/bot.js
+++ Discord/bot.js
@@ -1,17 +1,1 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
-client.once("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
-    if (message.content.toLowerCase() === "hello") {
-        message.reply("Hello! Welcome to Team Japanese.");
-    }
-});
-
-client.login(process.env.DISCORD_TOKEN);
